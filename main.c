5bd2e45941131267b4f1eee3e2db99c2aa749597
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2024-06-18 18:06:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,32 +3,67 @@
 
 int main() {
     int num1, num2;
-    printf("Enter two positive integers: ");
+    //printf("Enter two positive integers: ");
     scanf("%d %d", &num1, &num2);
-    
-    printf("Prime numbers: ");
-    for (int i = num1; i <= num2; i++) {
-        if (isPrime(i)) printf("%d ", i);
-    }
-    printf("\n");
 
-    printf("Armstrong numbers: ");
-    for (int i = num1; i <= num2; i++) {
-        if (isArmstrong(i)) printf("%d ", i);
-    }
-    printf("\n");
+    if(num2>num1){
 
-    printf("Strong numbers: ");
-    for (int i = num1; i <= num2; i++) {
-        if (isStrong(i)) printf("%d ", i);
-    }
-    printf("\n");
+        printf("The Armstrong numbers are:");
+        for (int i = num1; i <= num2; i++) {
+            if (isArmstrong(i)) printf(" %d", i);
+        }
+        printf("\n");
 
-    printf("Palindromes: ");
-    for (int i = num1; i <= num2; i++) {
-        if (isPalindrome(i)) printf("%d ", i);
-    }
-    printf("\n");
+        printf("The Palindromes are:");
+        for (int i = num1; i <= num2; i++) {
+            if (isPalindrome(i)) printf(" %d", i);
+        }
+        printf("\n");
 
-    return 0;
+        
+        printf("the Prime numbers are:");
+        for (int i = num1; i <= num2; i++) {
+            if (isPrime(i)) printf(" %d", i);
+        }
+        printf("\n");
+
+        printf("The Strong numbers are:");
+        for (int i = num1; i <= num2; i++) {
+            if (isStrong(i)) {
+                printf(" %d ", i);
+        }
+        printf("\n");
+        }
+    }
+    if(num1>num2){
+
+        printf("The Armstrong numbers are:");
+        for (int i = num2; i <= num1; i++) {
+            if (isArmstrong(i)) printf(" %d", i);
+        }
+        printf("\n");
+
+        printf("The Palindromes are:");
+        for (int i = num2; i <= num1; i++) {
+            if (isPalindrome(i)) printf(" %d", i);
+        }
+        printf("\n");
+
+        
+        printf("the Prime numbers are:");
+        for (int i = num2; i <= num1; i++) {
+            if (isPrime(i)) printf(" %d", i);
+        }
+        printf("\n");
+
+        printf("The Strong numbers are:");
+        for (int i = num2; i <= num1; i++) {
+            if (isStrong(i)) {
+                printf(" %d ", i);
+        }
+        printf("\n");
+
+        }
+
+    }
 }

b01819a02101820d7d683a041bd4941588124406
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2024-06-18 19:33:27 +0300

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int isPrime(int num) {
-    if (num <= 1) return 0;
+    if (num <= 1) return 1;
     if (num <= 3) return 1;
     if (num % 2 == 0 || num % 3 == 0) return 0;
     for (int i = 5; i * i <= num; i += 6) {

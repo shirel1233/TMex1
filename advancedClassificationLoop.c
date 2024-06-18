de17f6e7748c9dc45e00478a359731106dce8b9a
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2024-06-18 19:22:28 +0300

    Update advancedClassificationLoop.c

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,19 +1,27 @@
 #include "NumClass.h"
-#include <math.h>
+
+
+int my_pow(int base, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result *= base;
+    }
+    return result;
+}
 
 int isArmstrong(int num) {
-    int sum = 0, temp = num, remainder, n = 0;
+    int sum = 0, temp = num, digits = 0;
     while (temp != 0) {
         temp /= 10;
-        ++n;
+        digits++;
     }
     temp = num;
     while (temp != 0) {
-        remainder = temp % 10;
-        sum += pow(remainder, n);
+        int digit = temp % 10;
+        sum += my_pow(digit, digits);
         temp /= 10;
     }
-    return sum == num;
+    return (sum == num);
 }
 
 int isPalindrome(int num) {
